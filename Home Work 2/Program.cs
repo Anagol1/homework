7e3322d99f91f838137d55b3514265bcd1fe4da9
anastasia <nastas-golub@yandex.ru>
2022-10-28 16:28:16 +0300

    Task15 corrected

--- Home Work 2/Program.cs
+++ Home Work 2/Program.cs
@@ -1,5 +1,6 @@
 ﻿/*
-//Задача 10. Написать программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+//Задача 10. Написать программу, которая принимает на вход трехзначное число и на выходе 
+//показывает вторую цифру этого числа
 
 int FindSecondDigit (int num)
 {
@@ -12,28 +13,31 @@ Console.WriteLine("Input three-digit number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int digit2 = FindSecondDigit(num);
 Console.WriteLine(digit2);
-
+*/
 //Задача 13. Написать программу, которая выводит третью цифру заданного числа или сообщает, 
 //что третьей цифры нет.
-*/
+
 int FindThirdDigit (int num)
 {   
-    int result = -1;
     int num1 = num;
-    while (num > 999){   
-        num1 = num / 10;
-        num = num1;
-    }
-        int thirdDigit1 = num % 10;
-        result = thirdDigit1;
+    int result = 0;
     if (num < 100)
-       Console.WriteLine($"number digits of {num} less than 3");
-    
+        Console.WriteLine($"number digits of {num} less than 3");
+
     else if (num > 99 & num < 1000)
         {
         //Console.WriteLine($"Third digit of {num} is {num % 10}");
         int thirdDigit2 = num % 10; 
         result = thirdDigit2;}
+    else
+        
+        while (num > 999){   
+        num1 = num / 10;
+        num = num1;
+        }
+        int thirdDigit1 = num % 10;
+        result = thirdDigit1;
+    
     return result;    
 }
 Console.WriteLine("Input number: "); 
@@ -44,34 +48,23 @@ Console.WriteLine($"Third digit of {num} is {thirdDigit}");
 // не удалось сделать так, чтобы при num < 99 выводился только один результат. В чеи ошибка, подскажите, пожалуйста 
 /*
 
-//Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
 
-
-
-bool CheckDay (int dayNum)
+void CheckDay(int dayNum)
 {    
-    while (dayNum < 1 | dayNum > 7)
+    if (dayNum < 1 | dayNum > 7)
     {
         Console.WriteLine("Wrong number");
-        break; 
+        //break; 
     } 
     
-    if (dayNum == 6 | dayNum == 7) 
-    return true;
-    else return false;
-    
+    else if (dayNum == 6 | dayNum == 7) 
+    Console.WriteLine("$This is holliday");
+    else Console.WriteLine("$This is not holliday");
    }
 
 Console.WriteLine("Input day number: ");
 int dayNum = Convert.ToInt32(Console.ReadLine());
-
-bool result = CheckDay(dayNum);
-if (result)
-{
-    Console.WriteLine("$This is holliday");
-} 
-else
-{
-    Console.WriteLine("$This is not holliday");
-}
+CheckDay(dayNum);
 */
